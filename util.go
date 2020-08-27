e87fc8ec9f6836a80b5aa4bf197797b2815ee998
ptrbug <252910686@qq.com>
2020-08-27 15:31:07 +0800

    //modify

--- util.go
+++ util.go
@@ -7,24 +7,15 @@ import (
 )
 
 func isSymbolMatch(symbol string, name string) bool {
-	if len(symbol) < 5 {
-		return false
+	if len(symbol) >= 5 && symbol[0:5] == "SZ300" {
+		return true
 	}
-	if symbol[2:5] == "688" || symbol[2:5] == "787" || symbol[2:5] == "789" {
-		return false
-	}
-	if symbol[2:5] == "171" {
-		return false
-	}
-	if name[0:1] == "N" {
-		return false
-	}
-	return true
+	return false
 }
 
 func calcLimitUpPriceAndlimitUpPercent(lastClosePrice float64) (float64, float64) {
 	n10 := math.Pow10(2)
-	limitUpPrice := math.Trunc((lastClosePrice*1.1+0.5/n10)*n10) / n10
+	limitUpPrice := math.Trunc((lastClosePrice*1.2+0.5/n10)*n10) / n10
 	limitUpPercent := math.Trunc((limitUpPrice*100/lastClosePrice+0.5/n10)*n10) / n10
 	return limitUpPrice, limitUpPercent
 }
